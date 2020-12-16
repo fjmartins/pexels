b84acae9bb140ba57b6f0285c3e308deb056b83e
FJ <fabiomsjunior@gmail.com>
2020-12-16 21:02:16 +0900

    writing the base code

---
+++ app/src/main/java/com/fjmartins/pexels/ui/favorites/FavoritesFragment.kt
@@ -0,0 +1,31 @@
+package com.fjmartins.pexels.ui.favorites
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.fjmartins.pexels.R
+
+class FavoritesFragment : Fragment() {
+
+    private lateinit var favoritesViewModel: FavoritesViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        favoritesViewModel =
+                ViewModelProvider(this).get(FavoritesViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_favorites, container, false)
+        val textView: TextView = root.findViewById(R.id.text_gallery)
+        favoritesViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
