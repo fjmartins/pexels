8d5d508890b57c96548df7e36e89954d9b1222d8
FJ <fabiomsjunior@gmail.com>
2020-12-17 11:00:16 +0900

    removing unnecessary libs

--- app/src/main/java/com/fjmartins/pexels/dao/ImagesDao.kt
+++ app/src/main/java/com/fjmartins/pexels/dao/ImagesDao.kt
@@ -2,29 +2,28 @@ package com.fjmartins.pexels.dao
 
 import androidx.room.*
 import com.fjmartins.pexels.model.Image
-import io.reactivex.Single
 
 @Dao
 interface ImagesDao {
 
     @Query("SELECT * FROM images")
-    fun getAllImages(): Single<List<Image>>
+    fun getAllImages(): List<Image>
 
     @Insert
-    fun insertPhoto(currency: Image): Single<Long>
+    fun insertPhoto(currency: Image): Long
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertCurrencies(currencies: List<Image>): Single<List<Long>>
+    fun insertCurrencies(currencies: List<Image>): List<Long>
 
     @Query("SELECT * FROM images WHERE keyword=:keyword")
-    fun getCurrency(keyword: String): Single<Image>
+    fun getCurrency(keyword: String): Image
 
     @Update
-    fun updateCurrency(currencies: Image): Single<Int>
+    fun updateCurrency(currencies: Image): Int
 
     @Delete
-    fun deleteCurrency(currencies: Image): Single<Int>
+    fun deleteCurrency(currencies: Image): Int
 
     @Query("DELETE FROM images")
-    fun deleteAll(): Single<Int>
+    fun deleteAll(): Int
 }
